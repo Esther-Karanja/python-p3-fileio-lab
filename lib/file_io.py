514f37c12aa0e481f161616a1dcfe0a44d690d85
Esther Karanja <est.karanja@gmail.com>
2023-10-20 20:51:26 -0700

    completed lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
+import os
+
 def write_file(file_name, file_content):
-    pass
+    file_path = f'{file_name}.txt'
+    with open(file_path, 'w', encoding='utf-8')as x:
+     x.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+   file_path = f'{file_name}.txt'
+   with open(file_path, 'a', encoding='utf-8')as y:
+     y.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_path = f'{file_name}.txt'
+    with open(file_path, 'r', encoding='utf-8') as z:
+       return z.read()
